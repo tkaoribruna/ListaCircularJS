eb5022a5f9173a09a55821d37b219c121f21a3ea
Bruna Kaori Takuti <brunakaori@alunos.utfpr.edu.br>
2026-05-24 13:20:19 -0300

    Upload Lista Circular JavaScript

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log("Olá mundo e olá JavaScript");
+
+const nomeProjeto = "Estrutura de Dados - Lista Circular";
+let statusProjeto = "Em desenvolvimento";
+
+if (statusProjeto === "Em desenvolvimento") {
+    console.log(`Projeto 2: ${nomeProjeto} está avançando!`);
+}
+
+const somar = (a, b) => a + b;
+console.log("Teste de cálculo 2 + 3 = ", somar(2, 3));
